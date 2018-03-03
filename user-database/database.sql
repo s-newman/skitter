a0c887db9b61919d86a4fae4be6709c00d9678ed
Sean Newman <sxn6296@rit.edu>
2018-03-03 00:51:25 -0500

    Fix SQL script errors
    I am dumb, and messed up the syntax for a bunch of things in the SQL script.
    It works now!

--- user-database/database.sql
+++ user-database/database.sql
@@ -1,7 +1,19 @@
---- Create USER_INFO Table
+CREATE DATABASE users;
+USE users;
+
+-- Create PROFILE_PICTURE Table
+
+CREATE TABLE PROFILE_PICTURE (
+    picture_id int NOT NULL,
+    picture varbinary(65500) NOT NULL,
+    PRIMARY KEY (picture_id)
+);
+
+-- Create USER_INFO Table
+
 CREATE TABLE USER_INFO (
     user_id int NOT NULL,
-    username varchar(20) NOT NULL,
+    username varchar(15) NOT NULL,
     first_name varchar(15),
     last_name varchar(25),
     email varchar(254) NOT NULL,
@@ -12,18 +24,12 @@ CREATE TABLE USER_INFO (
     FOREIGN KEY (profile_picture_id) REFERENCES PROFILE_PICTURE(picture_id)
 );
 
---- Create PROFILE_PICTURE Table
-CREATE TABLE PROFILE_PICTURE (
-    picture_id int NOT NULL,
-    picture varbinary(max) NOT NULL,
-    PRIMARY KEY (picture_id)
-);
+-- Create FOLLOW Table
 
---- Create FOLLOW Table
 CREATE TABLE FOLLOW (
     follow_id int NOT NULL,
-    follower_user_id NOT NULL,
-    following_user_id NOT NULL,
+    follower_user_id int NOT NULL,
+    following_user_id int NOT NULL,
     PRIMARY KEY (follow_id),
     FOREIGN KEY (follower_user_id) REFERENCES USER_INFO(user_id),
     FOREIGN KEY (following_user_id) REFERENCES USER_INFO(user_id)
