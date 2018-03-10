d53f63d838741252430be71ed02a5569deb61a88
Sean Newman <sxn6296@rit.edu>
2018-03-09 23:44:34 -0500

    Added site-wide javascript
    A script has been added that contains helper functions to be used on a
    site-wide basis.  This script currently only has a function that gets a cookie
    of a specific name from the browser.

---
+++ frontend/app/ui/static/skitter.js
@@ -0,0 +1,22 @@
+/* A collection of helper functions for Skitter. */
+
+// Get a cookie from the browser based on the cookie's name
+function getCookie(name) {
+    // Cookie names all end with an equals sign
+    name = name + "=";
+
+    // Cookies are delimited by semicolons
+    let cookieArray = document.cookie.split(';');
+    for(cookie in cookieArray) {
+        // Check if the name occurs in the cookie
+        let nameLocation = cookie.indexOf(name);
+
+        // Name was found, return cookie value
+        if(nameLocation != -1) {
+            return cookie.substring(name.length + nameLocation, cookie.length - nameLocation);
+        }
+    }
+
+    // Cookie was not found
+    return "";
+}
