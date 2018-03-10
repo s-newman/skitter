fd2d4eb667de41b65670043675d0d694fba4a76f
Sean Newman <sxn6296@rit.edu>
2018-03-10 00:06:01 -0500

    Redirect user if they are already authenticated
    When logged-in users go to our website, we want them to be dropped at their
    dashboard, not the landing page!  A function has been added to grab the user's
    session cookie and check if they are authenticated, automatically redirecting
    them to the dashboard if they are.

--- frontend/app/ui/static/index.js
+++ frontend/app/ui/static/index.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
+    // Check if the user is authenticated already
+    let authCheckPost = $.post('/isAuthenticated', getCookie('SID'), function(data) {
+        let response = JSON.parseJSON(data);
+        if(response['status'] == 'authenticated') {
+            // user is auth'd, redirect em
+            authedUser();
+        }
+    });
+
     // Attempt to log in the user when they submit the login form
     $('#loginForm').submit(function(event) {
         // Create JSON string to send
@@ -6,14 +15,24 @@ $(document).ready(function () {
             username: $('#username').val(),
             password: $('#password').val()
         };
-
+        
         // Attempt to authenticate
         let loginPost = $.post('/login', postData);
 
         // Log to console if request failed
-        loginPost.fail(function(data) {
+        loginPost.fail(function() {
             console.log('Login attempt failed.');
-            console.log(data);
+            console.log(postData);
+        });
+
+        // Redirect to dashboard page if request was successful
+        loginPost.done(function() {
+            window.location.href('/dashboard');
         });
     });
 });
+
+// Redirect all authenticated users
+function authedUser() {
+    window.location.href = '/dashboard';
+}
