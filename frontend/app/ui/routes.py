9a44e023b20694eb4908312edb5c5bf383c1de46
Sean Newman <sxn6296@rit.edu>
2018-03-09 23:50:33 -0500

    Add dashboard page
    The dashboard page will eventually do actual stuff, but for now it's just a
    placeholder.

--- frontend/app/ui/routes.py
+++ frontend/app/ui/routes.py
@@ -4,3 +4,7 @@ from flask import render_template
 @app.route('/')
 def index():
     return render_template('index.html', scripts=True, jquery=True)
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
