fc47b698fc9b47ec0ba22fe8ca9013bf01b7f852
Sean Newman <sxn6296@rit.edu>
2018-03-14 17:53:59 -0400

    Remove test user authentication from index page
    The test user auth functions are a waste of time that makes testing more
    difficult.  Besides, redirection will happen at the API gateway level.

--- frontend/app/ui/static/js/index.js
+++ frontend/app/ui/static/js/index.js
@@ -1,14 +1,3 @@
-// Check if the user is authenticated already
-let authCheckPost = $.post('/isAuthenticated', getCookie('SID'), function(data) {
-    console.log('Checking if user is authenticated...');
-    let response = JSON.parseJSON(data);
-    if(response['status'] == 'authenticated') {
-        // user is auth'd, redirect em
-        console.log('User is authenticated, redirecting...');
-        authedUser();
-    }
-});
-
 $(document).ready(function () {
     // Attempt to log in the user when they submit the login form
     $('#loginForm').submit(function(event) {
@@ -29,15 +18,6 @@ $(document).ready(function () {
 
         // Log to console if request failed
         loginPost.fail(function() {
-            /* Test user authentication
-               * * * * * * * * * * *
-               * REMOVE FROM PROD  *
-               * * * * * * * * * * *
-            */
-            if(postData.username === 'testuser' && postData.password === 'password') {
-                console.log('Authentication with test user accepted.');
-                authedUser();
-            }
             // Display error to user
             $('#errorText').html('Invalid credentials.');
             $('#username').css('border-color', '#ff0000');
