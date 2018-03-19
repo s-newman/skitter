630851bf7dcb73f04b4c452fcc1b4039df496b06
Sean Newman <sxn6296@rit.edu>
2018-03-19 02:23:14 -0400

    Implement username changing function
    It's pretty basic, but it checks if a username is taken before changing it.
    Rate-limiting controls must be implemented later.

--- frontend/app/ui/static/js/settings-page.js
+++ frontend/app/ui/static/js/settings-page.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
+    // Submit username change
+    $('#change-username').submit(function(event) {
+        // Clear error text
+        $('#username-error-text').empty();
 
+        // Check if username is in use
+        console.log('Checking if username is available.');
+        let usernameGet = $.get('/username', { username: $('#username').val() }, function(data) {
+            // If username is not taken, change it
+            if(!data.taken) {
+                console.log('Username is available, changing...');
+                $.put('/changeDisplayName', { place: 'holder' });
+            } else {
+                // Username is taken, display error message
+                console.log('Username is unavailable.');
+                $('#username-error-text').html('Username is taken.');
+            }
+        });
+
+        // Log errors from previous request
+        usernameGet.fail(function() {
+            console.log('Could not check if username ' + $('#username').val() + ' is available.');
+        });
+    });
+
+    // Submit password change
+    $('#change-password').submit(function(event) {
+
+    });
+
+    // Submit profile picture change
+    $('#change-profile-pic').submit(function(event) {
+
+    });
 });
