50d02f8ef24767083142685504fa404a76731ff7
Sean Newman <sxn6296@rit.edu>
2018-03-19 03:08:15 -0400

    Finish profile picture change function
    It's done, I think.  It's probably bad code but I'll catch it later when I
    write tests.

--- frontend/app/ui/static/js/settings-page.js
+++ frontend/app/ui/static/js/settings-page.js
@@ -15,17 +15,37 @@ $(document).ready(function() {
                 // Username is taken, display error message
                 console.log('Username is unavailable.');
                 $('#username-error-text').html('Username is taken.');
+                event.preventDefault();
             }
         });
 
         // Log errors from previous request
         usernameGet.fail(function() {
             console.log('Could not check if username ' + $('#username').val() + ' is available.');
+            event.preventDefault();
         });
     });
 
     // Submit profile picture change
     $('#change-profile-pic').submit(function(event) {
+        let fileArray = document.getElementById('picture-upload').files;
 
+        // Continue only if the most recently uploaded file is small enough
+        if(checkPictureSize(fileArray[fileArray.length - 1])) {
+            console.log('Changing profile picture.');
+            
+            // Change image
+            let putImage = $.put('/changeProfileImage', { place: 'holder'}, function(data) {
+                console.log('Successfully changed photo.');
+            });
+
+            // Log errors
+            putImage.fail(function() {
+                console.log('Encountered an error when changing photo.');
+                event.preventDefault();
+            });
+        } else {
+            event.preventDefault();
+        }
     });
 });
--- frontend/app/ui/static/js/settings.js
+++ frontend/app/ui/static/js/settings.js
@@ -4,6 +4,8 @@
  * @param {File} file: The picture file to check.
  * @param {Element} error: The div element which will contain any error text to
  * be displayed.
+ * @returns {boolean}: True if the file is within the limits, false if the file
+ * is too large.
  */
 function checkPictureSize(file, error) {
     // Clear the error text
@@ -13,5 +15,9 @@ function checkPictureSize(file, error) {
     if(file.size > 1000000) {
         console.log('File is too large!');
         error.html('File is too large!');
+        return false;
+    } else {
+        // File is small enough
+        return true;
     }
 }
