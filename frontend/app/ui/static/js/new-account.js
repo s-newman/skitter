fd99fac161dd56f0b267c22018818aae097a7751
Sean Newman <sxn6296@rit.edu>
2018-03-27 01:23:59 -0400

    Update new account page to simply ask for username
    That's all that really needs to be done.  Everything else will be defaults.

--- frontend/app/ui/static/js/new-account.js
+++ frontend/app/ui/static/js/new-account.js
@@ -1,57 +1,15 @@
 $(document).ready(function() {
-    // Detect if file is too big
-    $('#picture-upload').bind('change', function() {
-        let fileArray = document.getElementById('picture-upload').files;
-
-        checkPictureSize(fileArray[fileArray.length - 1].size, $('#upload-error-text'));
-    });
-
     // Create the new user
     $('#new-acct-form').submit(function(event) {
-        // Track whether there were any errors
-        let error = false;
-
         // Clear the error text
         $('#form-error-text').empty();
 
         // Check if all forms have been filled
-        if($('#username').val() === '' ||
-           $('#first-password').val() === '' ||
-           $('#second-password').val() === '' ||
-           $('#picture-upload').val() === '' ||
-           $('#email').val() === '') {
-            console.log('Not all form fields are filled.');
-            $('#form-error-text').html('Please fill out all form fields!<br />');
-            error = true;
+        if($('#username').val() === '') {
+            console.log('The username has not been entered.');
+            $('#form-error-text').html('Please pick a username!<br />');
             event.preventDefault();
         }
-
-        // Only allow submission if the file is small enough
-        try {
-            // Attempt the thing
-            if(document.getElementById('picture-upload').files[0].size > 1000000) {
-                $('#form-error-text').append('File upload is too large.');
-                event.preventDefault();
-                error = true;
-            }
-        } catch (e) {
-            // Handle the exception if no file has been uploaded
-            if(e instanceof TypeError) {
-                $('#form-error-text').append('Please upload a profile picture.');
-                event.preventDefault();
-                error = true;
-            } else {
-                // This shouldn't happen, let it bubble
-                throw e;
-            }
-        }
-
-        // Display information if there were any problems
-        if(error) {
-            console.log('Please resolve errors.');
-            $('#form-error-text').prepend('Please correct the following errors:<br />');
-        }
-
         // Allow the form to be submitted
     });
 });
