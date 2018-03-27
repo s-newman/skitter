a3b3e7ebea86c386faca20ebf91b25a671bfe427
Sean Newman <sxn6296@rit.edu>
2018-03-27 01:47:51 -0400

    Install selenium webdrivers in Travis
    For the selenium scripts to work, the webdrivers have to be installed.  Let's
    do that.

---
+++ testing/get-webdrivers.sh
@@ -0,0 +1,8 @@
+wget
+https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar
+.gz
+tar -xzvf geckodriver-v0.11.1-linux64.tar.gz
+mv geckodriver /bin
+wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+mv chromedriver /bin
