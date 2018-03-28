0636ca1a8a6331f51bcb80e34171c2ce5f1b610e
Sean Newman <sxn6296@rit.edu>
2018-03-28 15:41:53 -0400

    Use updated geckodriver
    Why was I using version 0.11.1, when the most updated version of the
    geckodriver is version 0.20.0?  Come on, man!

--- testing/get-webdrivers.sh
+++ testing/get-webdrivers.sh
@@ -1,5 +1,5 @@
-wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
-tar -xzvf geckodriver-v0.11.1-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.20.0/geckodriver-v0.20.0-linux64.tar.gz
+tar -xzvf geckodriver-v0.20.0-linux64.tar.gz
 mv geckodriver /bin
 wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
