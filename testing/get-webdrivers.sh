656b8d4fd232bcff911495bd50f2c01f8fba64be
Sean Newman <sxn6296@rit.edu>
2018-03-28 14:22:11 -0400

    Fix script formatting error
    For some reason a line break was added to the get-webdrivers script, which
    prevented one of the drivers from being downloaded.  This has been fixed.

--- testing/get-webdrivers.sh
+++ testing/get-webdrivers.sh
@@ -1,6 +1,4 @@
-wget
-https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar
-.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.11.1/geckodriver-v0.11.1-linux64.tar.gz
 tar -xzvf geckodriver-v0.11.1-linux64.tar.gz
 mv geckodriver /bin
 wget https://chromedriver.storage.googleapis.com/2.37/chromedriver_linux64.zip
