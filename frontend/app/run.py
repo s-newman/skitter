03dc2c80ecb360a45a79130836aa8efc2d3e7f54
Sean Newman <sxn6296@rit.edu>
2018-03-30 12:09:16 -0400

    Add default profile picture to database on startup
    The frontend flask app will now add the default profile picture to the database
    upon startup, or at least attempt to.

--- frontend/app/run.py
+++ frontend/app/run.py
@@ -1,1 +1,22 @@
 from ui import app
+import MySQLdb
+
+# Create mySQL connection
+connection = None
+while connection is None:
+    try:
+        connection = MySQLdb.connect(host='user-db', user='api-gateway',
+                                     passwd='changemeplease-securitysucks',
+                                     db='users')
+    except:
+        pass
+
+c = connection.cursor()
+
+# Open file
+with open('ui/static/img/default-profile.png') as f:
+    picture = f.read()
+
+# Insert file to database
+hex_picture = '0x{}'.format(binascii.hexlify(image).decode('ascii'))
+c.execute('INSERT INTO PROFILE_PICTURE VALUES (0, %s)', (hex_picture,))
