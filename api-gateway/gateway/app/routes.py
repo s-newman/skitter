1ffba502878277721d764ec8212075b3cca10ef8
Sean Newman <sxn6296@rit.edu>
2018-03-30 01:59:12 -0400

    Implement authentication endpoints
    The authentication endpoints are now implemented, along with some helper
    functions.

--- api-gateway/gateway/app/routes.py
+++ api-gateway/gateway/app/routes.py
@@ -2,6 +2,7 @@ from app import app
 from app.config import *
 from flask import request, Response, abort
 import requests
+import json.loads
 
 CHUNK_SIZE = 1024
 """The size, in bytes, of data to stream at a time."""
@@ -27,43 +28,65 @@ def frontend(page=None, filename=None, user=None):
                         it out) whose profile page is being loaded.
     :return:            The resultant page, streamed back to the client.
     """
-    r = get_response(FRONTEND, request.path)
-    headers = dict(r.headers)
+    r = get_response(FRONTEND, request.path, 'GET')
+    return make_response(r)
 
+
+@app.route('/isAuthenticated')
+@app.route('/signIn', methods=['POST'])
+@app.route('/newUser')
+@app.route('/deleteUser')
+def authentication():
+    if request.method == 'GET':
+        r = get_response(AUTH, request.full_path, 'GET')
+    elif request.method == 'POST':
+        r = get_response(AUTH, request.path, 'POST', request.data)
+
+    return make_response(r)
+
+
+def make_response(r):
+    headers = dict(r.headers)
     def generate():
         for chunk in r.iter_content(CHUNK_SIZE):
             yield chunk
     return Response(generate(), headers=headers)
 
 
-def get_response(host, method):
+def get_response(host, method, http_method, data=None):
     """Make a given request and return the associated response.
 
-    :param host:    A string; the hostname of the microservice endpoint to send
-                    the request to, including the destination port.  Should be
-                    formatted like:
-                        frontend:8000
-    :param method:  A string; the API method to send to the microservice
-                    endpoint.  This string SHOULD NOT include the leading
-                    forward slash.  For example, a request to /login should be
-                    passed as:
+    :param host: A string; the hostname of the microservice endpoint to send
+                 the request to, including the destination port.  Should be
+                 formatted like:
+                    frontend:8000
+                 If parameters are included in the request (for example, in the
+                 case of a GET request), they should be included in the string.
+    :param method: A string; the API method to send to the microservice
+                   endpoint.  This string SHOULD NOT include the leading
+                   forward slash.  For example, a request to /login should be
+                   passed as:
                         login
-    :return:        The response recieved for the given request.  Uses
-                    streaming to send back requests as they are received to the
-                    client.
+    :param http_method: A string; the HTTP method to use to send the request to
+                        the microservice endpoint.
+    :param data: A string; the body of the request.  Should only be set if the
+                 request method is POST.
+    :return: The response recieved for the given request.  Uses
+             streaming to send back requests as they are received to the
+             client.
     """
     # TODO: add HTTPS support
     url = 'http://{}{}'.format(host, method)
     # Fetch the URL and stream it back
     # return requests.get(url, stream=True, params=request.args)
-    return requests.get(url)
+    if http_method == 'GET':
+        return requests.get(url)
+    
+    elif http_method == 'POST':
+        return requests.post(url, json=json.loads(data))
 
 
 # Optional methods are not included in this list.
-@app.route('/isAuthenticated')
-@app.route('/signIn')
-@app.route('/newUser')
-@app.route('/deleteUser')
 @app.route('/changeDisplayName')
 @app.route('/changeProfileImage')
 @app.route('/AddSkit')
