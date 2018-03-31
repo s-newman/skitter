288b24b38b0295761fe57d034c86d191757d6ba1
Sean Newman <sxn6296@rit.edu>
2018-03-30 21:17:32 -0400

    Replace unoffical mysql connector with official
    Because the other one has terrible documentation and it was hard to figure out
    how to get it to work.  The reason things aren't working are probably not the
    fault of the other connector but w/e.

--- frontend/app/run.py
+++ frontend/app/run.py
@@ -1,25 +1,27 @@
 from ui import app
 from time import sleep as wait
-import MySQLdb
+import mysql.connector
 from binascii import hexlify
 
 # Create mySQL connection
-connection = None
-while connection is None:
+cnx = None
+while cnx is None:
     try:
-        connection = MySQLdb.connect(host='user-db', user='api-gateway',
-                                     passwd='changemeplease-securitysucks',
-                                     db='users')
+        cnx = mysql.connector.connect(host='user-db', user='api-gateway',
+                                      passwd='changemeplease-securitysucks',
+                                      db='users')
     except Exception as e:
         wait(3)
         print(str(e))
 
-c = connection.cursor()
+c = cnx.cursor()
 
 # Open file
 with open('ui/static/img/default-profile.png', 'rb') as f:
     picture = f.read()
 
 # Insert file to database
-hex_picture = '0x{}'.format(hexlify(picture).decode('ascii'))
 c.execute('INSERT INTO PROFILE_PICTURE VALUES (0, %s)', (hex_picture,))
+
+# Close connection
+cnx.close()
