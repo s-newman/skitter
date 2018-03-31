e1bb1b6fbf3e1d234df120255961fbe1eaa7a141
Sean Newman <sxn6296@rit.edu>
2018-03-31 18:26:11 -0400

    Refactor authentication into other function
    Because both buttons require the user to be authenticated, refactoring it out
    will help.

--- frontend/app/ui/static/js/index.js
+++ frontend/app/ui/static/js/index.js
@@ -1,49 +1,69 @@
 $(document).ready(function () {
     // Attempt to log in the user when they submit the login form
-    $('#loginForm').submit(function(event) {
-        // Clear error
-        $('#errorText').empty();
-        $('#username').removeClass('error');
-        $('#password').removeClass('error');
-        
-        // Create JSON string to send
-        let postData = JSON.stringify({
-            username: $('#username').val(),
-            password: $('#password').val()
-        });
-        console.log('Attempting to authenticate user:', postData);
-
-        // Attempt to authenticate
-        postJSON('/signIn', postData, function(data) {
-            // Authentication was successful
-            if(data.successful === 'true') {
-                console.log('Authentication successful.');
-                setCookie('SID', data.sessionID, 1);
-                authedUser();
-            }
-
-            // Authentication failed
-            else {
-                $('#errorText').html('Invalid credentials.');
-                $('#username').addClass('error');
-                $('#password').addClass('error');
-                console.log('Login attempt failed.');
-            }
-        }, function() {
-            // The thing failed.
-            console.log('There was an error with your request.');
-        });
-
+    $('#loginForm').submit(function() {
+        if(authenticate()) {
+            window.location.href = '/dashboard';
+        }
         event.preventDefault();
     });
-
+    
     $('#sign-up').click(function(event) {
-        // We don't want the form to be submitted (yet)!
+        if(authenticate()) {
+            console.log('signing up...');
+            window.location.href = '/new-account';
+        }
         event.preventDefault();
-        console.log('signing up...');
-        window.location.href = '/new-account';
     });
 });
+
+function authenticate() {
+    // Check if the user is already authenticated
+    let authCheck = JSON.stringify({
+        username: $('#username').val()
+    });
+    postJSON('/isAuthenticated', authCheck, function(data) {
+        // Already authenticated, return
+        if(data.authenticated === 'true') {
+            return true;
+        }
+        // PLS FIX DUC THX
+    });
+
+    // Clear error
+    $('#errorText').empty();
+    $('#username').removeClass('error');
+    $('#password').removeClass('error');
+    
+    // Create JSON string to send
+    let postData = JSON.stringify({
+        username: $('#username').val(),
+        password: $('#password').val()
+    });
+    console.log('Attempting to authenticate user:', postData);
+    
+    // Attempt to authenticate
+    postJSON('/signIn', postData, function(data) {
+        // Authentication was successful
+        if(data.successful === 'true') {
+            console.log('Authentication successful.');
+            setCookie('SID', data.sessionID, 1);
+            return true;
+        }
+    
+        // Authentication failed
+        else {
+            $('#errorText').html('Invalid credentials.');
+            $('#username').addClass('error');
+            $('#password').addClass('error');
+            console.log('Login attempt failed.');
+            return false;
+        }
+    }, function() {
+        // The thing failed.
+        console.log('There was an error with your request.');
+        return false;
+    });
+}
 
 // Redirect all authenticated users
 function authedUser() {
