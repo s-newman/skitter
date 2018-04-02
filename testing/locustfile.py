b837615c1b99b51b5d6cf4fbe9110a11fca51b16
Sean Newman <sxn6296@rit.edu>
2018-04-02 18:59:16 -0400

    Update locust tests
    Now, we'll have some real hardcore serious testing going on.  I wonder if we
    can handle the load?

--- testing/locustfile.py
+++ testing/locustfile.py
@@ -1,31 +1,70 @@
 from locust import HttpLocust, TaskSet, task
 from random import randint
+from json import dumps
 
 
-class Tests(TaskSet):
+# Ensure that unauthenticated users get 401 errors for internal pages
+class UnauthenticatedTests(TaskSet):
     @task
-    def index(self):
-        self.client.get('/')
+    def dashboard(self):
+        internal_page(self, '/dashboard')
 
     @task
     def settings(self):
-        self.client.get('/settings')
+        internal_page(self, '/settings')
+    
+    @task
+    def new_account(self):
+        internal_page(self, '/new-account')
+    
+    @task
+    def profile(self):
+        page_id = randint(1, 1000)
+        internal_page(self, '/profile/{}'.format(page_id))
 
+
+class InternalTests(TaskSet):
+
+    def on_start(self):
+        username = 'test{}'.format(randint(1, 500))
+        self.client.get('/')
+        self.client.get('/isAuthenticated?username={}'.format(username))
+        self.client.post('/signIn', json=dumps({
+            'username': username,
+            'password': 'fakenews'
+        }))
+    
+    def on_stop(self):
+        self.client.get('/logout')
+    
     @task
     def dashboard(self):
         self.client.get('/dashboard')
-
+    
+    @task
+    def settings(self):
+        self.client.get('/settings')
+    
     @task
     def new_account(self):
         self.client.get('/new-account')
-
+    
     @task
     def profile(self):
-        self.client.get('/profile/{}'.format(randint(1, 1000)))
+        page_id = randint(1, 1000)
+        self.client.get('/profile/{}'.format(page_id))
 
-    @task
-    def logout(self):
-        self.client.get('/logout')
+
+class Tests(TaskSet):
+    tasks = {
+        InternalTests: 7,
+        UnauthenticatedTests: 2
+    }
+
+    @task(1)
+    def index(self):
+        self.client.get('/')
+    
 
 
 class WebsiteUser(HttpLocust):
@@ -33,3 +72,18 @@ class WebsiteUser(HttpLocust):
     min_wait = 1000
     max_wait = 10000
     host = 'http://localhost'
+
+
+def internal_page(self, page):
+    """Ensure that an unauthenticated user recieves a 401 error for internal
+    pages
+    
+    Arguments:
+        page {string} -- The URI of the page to check
+    """
+
+    with self.client.get(page, catch_response=True) as response:
+        if response.status_code == 401:
+            response.success()
+        else:
+            response.failure('Did not return 401 error.')
