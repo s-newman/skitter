8faa37db82c50c586a6b475a8679afb198805cec
Sean Newman <sxn6296@rit.edu>
2018-04-08 12:20:17 -0400

    Fix errors with creating test users
    MySQL scripts are weird.

--- user-database/database.sql
+++ user-database/database.sql
@@ -17,6 +17,7 @@ CREATE TABLE USER_INFO (
     first_name varchar(15),
     last_name varchar(25),
     email varchar(254) NOT NULL,
+    session_id varchar(40),
     private_account bool,
     profile_picture_id int NOT NULL,
     PRIMARY KEY (rit_username),
@@ -26,9 +27,10 @@ CREATE TABLE USER_INFO (
 -- Create FOLLOW Table
 
 CREATE TABLE FOLLOW (
+    follow_id int NOT NULL,
     follower_user_id varchar(7) NOT NULL,
     following_user_id varchar(7) NOT NULL,
-    PRIMARY KEY (follower_user_id, following_user_id),
+    PRIMARY KEY (follow_id),
     FOREIGN KEY (follower_user_id) REFERENCES USER_INFO(rit_username),
     FOREIGN KEY (following_user_id) REFERENCES USER_INFO(rit_username)
 );
@@ -51,16 +53,20 @@ INSERT INTO PROFILE_PICTURE (picture_id, picture) VALUES (
 SET GLOBAL general_log=1;
 
 -- Add users
-DROP PROCEDURE IF EXISTS makeuser();
 DELIMITER //
 CREATE PROCEDURE makeuser()
 BEGIN
     DECLARE start INT DEFAULT 0;
-    WHILE start <= 999 DO:
-        INSERT INTO USER_INFO (rit_username, first_name, last_name, email, profile_picture_id)
-        VALUES (concat('test', start), 'Test', 'User', concat('test', start, '@rit.edu'), 0);
+    WHILE start <= 999 DO
+        INSERT INTO USER_INFO (rit_username, first_name, last_name, email, profile_picture_id) VALUES (
+            concat('test', start),
+            'Test',
+            'User',
+            concat('test', start, '@rit.edu'),
+            0
+        );
         SET start = start +1;
     END WHILE;
-END //
+END//
 DELIMITER ;
-CALL PROCEDURE makeuser;
+CALL makeuser;
