a2e4460da0dc27efc7e23928ceb8239083bdd0b5
Sean Newman <sxn6296@rit.edu>
2018-04-19 23:53:45 -0400

    Implement /followState endpoint
    To check if you've followed a user.

--- follow-users/app/following/routes.py
+++ follow-users/app/following/routes.py
@@ -96,6 +96,39 @@ def follow():
     return jsonify({'success':True})
 
 
+@app.route('/followState')
+def check_follow():
+    # Parse the request data
+    follow = request.args['follow']
+
+    cnx = connect_db()
+
+    # Check that the user is logged in
+    cnx.execute('PREPARE check_auth FROM ' +
+                '\'SELECT * FROM SESSION WHERE session_id = ?\';')
+    cnx.execute('SET @a = \'{}\';'.format(request.cookies['SID']))
+    results = [r for r in cnx.execute('EXECUTE check_auth USING @a;')]
+    
+    # There should only be one entry for that session ID
+    if len(results) != 1:
+        return jsonify({'success':False})
+    
+    # Save the username
+    username = results[0][0]
+
+    # Check if the user is already followed
+    cnx.execute('PREPARE check_follow FROM ' +
+                '\'SELECT * FROM FOLLOW\n' +
+                'WHERE follower = ? AND followed = ?\';')
+    cnx.execute('SET @a = \'{}\';'.format(username))
+    cnx.execute('SET @b = \'{}\';'.format(follow))
+    results = [r for r in cnx.execute('EXECUTE check_follow USING @a, @b;')]
+    if len(results) > 0:
+        return jsonify({'success':True})
+    else:
+        return jsonify({'success':False})
+
+
 @app.route('/unfollowUser')
 def unfollow():
     return None
